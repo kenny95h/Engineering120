88ab8e58edcc72b5d4d87c6cb90e5a5ea82a1b9d
Kenny Harvey <kenny95h@hotmail.com>
2022-10-04 08:57:48 +0100

    Projects updates with notes

--- PythonProjects/REST_API_Testing/starter_code/tests/integration/models/test_item.py
+++ PythonProjects/REST_API_Testing/starter_code/tests/integration/models/test_item.py
@@ -2,6 +2,7 @@ from starter_code.models.item import ItemModel
 from starter_code.tests.base_test import BaseTest
 
 
+# inherits from the BaseTest class to create the blank database
 class ItemTest(BaseTest):
     def test_crud(self):
         with self.app_context(): # calls the setup of the app from the base test
@@ -12,3 +13,7 @@ class ItemTest(BaseTest):
             item.save_to_db() # calls method to save item to db
 
             self.assertIsNotNone(ItemModel.find_by_name('test')) # checks db can find new item.
+
+            item.delete_from_db() # confirm the delete function of the db works
+
+            self.assertIsNone(ItemModel.find_by_name('test')) # checks the item can no longer be found
--- PythonProjects/REST_API_Testing/starter_code/tests/unit/models/test_item.py
+++ PythonProjects/REST_API_Testing/starter_code/tests/unit/models/test_item.py
@@ -4,15 +4,21 @@ from starter_code.models.item import ItemModel
 
 class ItemTest(TestCase):
     def test_create_item(self):
+        # create an instance of the item model
         item = ItemModel("test", 19.99)
 
+        # check both variables have been instantiated as expected
         self.assertEqual(item.name, "test")
         self.assertEqual(item.price, 19.99)
 
     def test_item_json(self):
         item = ItemModel("test", 19.99)
+
+        # the expected json item from calling the method
         expected = {
             "name": "test",
             "price": 19.99
         }
+
+        # check the json method returns the item as a dictionary
         self.assertEqual(item.json(), expected)
